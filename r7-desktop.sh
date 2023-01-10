55373c4a299058f6353f49974ddc7cf381ebacb1
Alexander Shubenkov <alshubenkov@yandex.ru>
2023-01-10 15:33:10 +0300

    release 0.0.7

--- r7-desktop.sh
+++ r7-desktop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+exec 2>&1 | tee file.log
 err() { echo -e "* Error: $1"; exit 1; }
 
 echo "Скрипт автоматической установки R7-Office Desktop"
@@ -12,19 +13,47 @@ os_type=`cat /etc/*-release | grep ^ID= | tr -d '"' | awk '{print substr ($ 0, 4
 echo "$os_type"
 
 case "$os_type" in
-
+######## Блок кода для Астра
  "astra" )
  echo "ОПределена ОС семейства Астра Линукс"
+ echo "Запуск обновления системы"
+ sudo apt-get update -y
+ echo "Установка зависимостей"
+ sudo apt-get install -y fonts-crosextra-carlito fonts-dejavu fonts-liberation fonts-opensymbol curl gstreamer1.0-libav gstr$
+ echo "Скачивание дистрибутива"
+ wget -N https://download.r7-office.ru/astra/r7-office.deb
+ echo "Установка r7 desktop"
+ sudo dpkg -i  r7-office.deb
  ;;
+######### Конец блока кода для Астра
 
+######## Блок кода для РЕДОС
  "redos" )
  echo "ОПределена ОС семейства РЕДОС Линукс"
+ #Установка Р7-Десктоп на РЕДОС
+ # Обновление системы
+ echo "Запуск обновления системы"
+ sudo yum update -y
+ echo "Установка зависимостей"
+ sudo yum install -y http://li.nux.ro/download/nux/dextop/el7/x86_64//gstreamer1-libav-1.0.6-1.el7.nux.x86_64.rpm
+ echo "Скачивание дистрибутива"
+ wget  -N https://download.r7-office.ru/centos/r7-office.rpm
+ echo "Установка r7 desktop"
+ sudo yum install -y r7-office.rpm
  ;;
+######### Конец блока кода для РЕДОС
 
+######### Блок кода для РОСА
  "rosa" )
  echo "ОПределена ОС семейства РОСА Линукс"
+ echo "Запуск обновления системы"
+ sudo urpmi --auto-update --auto
+ echo "Установка r7 desktop"
+ sudo urpmi --auto https://download.r7-office.ru/rosa/r7-office.rpm
  ;;
+######### Конец блока кода для РОСА
 
+######### Блок кода для Альт
  "altlinux" )
  echo "Определена ОС семейства АЛЬТ Линукс"
  echo ""
@@ -49,9 +78,8 @@ case "$os_type" in
  wget https://download.r7-office.ru/altlinux/r7-office.rpm || err "ошибка wget https://download.r7-office.ru/altlinux/r7-office.rpm"
  echo "Установка r7-office desktop" 
  rpm -i r7-office.rpm || err "ошибка установки r7-office.rpm"
- echo "Выход из учетной записи ROOT"
- exit
+ echo "Установка завершена успешно"
  echo "Запустите Р7 Офис Десктоп, не забудьте установить лицензию"
  ;;
-
+######### Конец блока кода для Альт
 esac
