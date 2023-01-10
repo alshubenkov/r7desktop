d282a1a35e0f250f1b4dd99a2aa48a1268d09365
Alexander Shubenkov <alshubenkov@yandex.ru>
2023-01-10 20:14:08 +0300

    release 0.0.9

--- r7-desktop.sh
+++ r7-desktop.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
+set -e
+set -o pipefail -e
+# Запись лог файла установки
 exec > >(tee file.log) 2>&1
+# Функция для выхода из скрипта при ошибке
+err()
+    {
+    echo -e "* Error: $1"
+    exit 1
+    }
 
-err() { echo -e "* Error: $1"; exit 1; }
 
 echo "Скрипт автоматической установки R7-Office Desktop"
 echo ""
@@ -11,22 +18,29 @@ echo ""
 
 os_type=`cat /etc/*-release | grep ^ID= | tr -d '"' | awk '{print substr ($ 0, 4)}'`
 
+echo "Определена ОС Линукс семейства:"
 echo "$os_type"
+
+echo "Проверка связи с сервером download.r7-office.ru"
+ping -c 5 download.r7-office.ru || err "Нет связи с сервером загрузок Р7, проверьте настройки соединения"
+
 
 case "$os_type" in
 ######## Блок кода для Астра
  "astra" )
  echo "ОПределена ОС семейства Астра Линукс"
  echo "Запуск обновления системы"
- sudo apt-get update -y || err "ошибка apt-get update -y"
+ sudo apt-get update -y && sudo apt-get dist-upgrade -y
  echo "Установка зависимостей"
  sudo apt-get install -y fonts-crosextra-carlito fonts-dejavu fonts-liberation fonts-opensymbol \
  curl gstreamer1.0-libav gstreamer1.0-plugins-ugly libasound2 libc6 libcairo2 libgcc1 libgconf-2-4 \
- libgtk-3-0 libstdc++6 libx11-6 libxss1 x11-common xdg-utils || err "ошибка установки зависимостей"
+ libgtk-3-0 libstdc++6 libx11-6 libxss1 x11-common xdg-utils 
  echo "Скачивание дистрибутива"
- wget -N https://download.r7-office.ru/astra/r7-office.deb || err "ошибка загрузки https://download.r7-office.ru/astra/r7-office.deb"
+ wget -N https://download.r7-office.ru/astra/r7-office.deb 
  echo "Установка r7 desktop"
- sudo dpkg -i  r7-office.deb || err "ошибка установки r7-office.deb"
+ sudo dpkg -i -E r7-office.deb
+ echo "Установка завершена успешно"
+ echo "Запустите Р7 Офис Десктоп, не забудьте установить лицензию"
  ;;
 ######### Конец блока кода для Астра
 
@@ -36,13 +50,13 @@ case "$os_type" in
  #Установка Р7-Десктоп на РЕДОС
  # Обновление системы
  echo "Запуск обновления системы"
- sudo yum update -y || err "ошибка yum update -y"
+ sudo yum update -y 
  echo "Установка зависимостей"
- sudo yum install -y http://li.nux.ro/download/nux/dextop/el7/x86_64//gstreamer1-libav-1.0.6-1.el7.nux.x86_64.rpm || err "ошибка установки зависимостей"
+ sudo yum install -y http://li.nux.ro/download/nux/dextop/el7/x86_64//gstreamer1-libav-1.0.6-1.el7.nux.x86_64.rpm 
  echo "Скачивание дистрибутива"
- wget  -N https://download.r7-office.ru/centos/r7-office.rpm || err "ошибка загрузки https://download.r7-office.ru/centos/r7-office.rpm"
+ wget  -N https://download.r7-office.ru/centos/r7-office.rpm 
  echo "Установка r7 desktop"
- sudo yum install -y r7-office.rpm || err "ошибка установки r7-office.rpm"
+ sudo yum install -y r7-office.rpm 
  echo "Установка завершена успешно"
  echo "Запустите Р7 Офис Десктоп, не забудьте установить лицензию"
  ;;
@@ -52,9 +66,9 @@ case "$os_type" in
  "rosa" )
  echo "ОПределена ОС семейства РОСА Линукс"
  echo "Запуск обновления системы"
- sudo urpmi --auto-update --auto || err "ошибка urpmi --auto-update --auto"
+ sudo urpmi --auto-update --auto
  echo "Установка r7 desktop"
- sudo urpmi --auto https://download.r7-office.ru/rosa/r7-office.rpm || err "ошибка установки r7-office.rpm"
+ sudo urpmi --auto https://download.r7-office.ru/rosa/r7-office.rpm
  echo "Установка завершена успешно"
  echo "Запустите Р7 Офис Десктоп, не забудьте установить лицензию"
  ;;
@@ -78,15 +92,18 @@ case "$os_type" in
   fi
  # 
  echo "Запуск обновления пакетов"
- apt-get update -y || err "ошибка apt-get update -y"
+ apt-get update -y && apt-get dist-upgrade -y
  echo "Установка зависимостей"
- apt-get install -y wget fonts-ttf-dejavu fonts-ttf-google-crosextra-carlito fonts-ttf-liberation glibc gst-libav gst-plugins-ugly1.0 libX11 libXScrnSaver libcairo libgcc1 libgtk+2 libgtkglext || err "ошибка apt-get install -y"
+ apt-get install -y wget fonts-ttf-dejavu fonts-ttf-google-crosextra-carlito fonts-ttf-liberation \
+ glibc gst-libav gst-plugins-ugly1.0 libX11 libXScrnSaver libcairo libgcc1 libgtk+2 libgtkglext 
  echo "Получение скрипта из репозитория Р7"
- wget https://download.r7-office.ru/altlinux/r7-office.rpm || err "ошибка wget https://download.r7-office.ru/altlinux/r7-office.rpm"
+ wget -N https://download.r7-office.ru/altlinux/r7-office.rpm
  echo "Установка r7-office desktop" 
- rpm -i r7-office.rpm || err "ошибка установки r7-office.rpm"
+ rpm -i r7-office.rpm
  echo "Установка завершена успешно"
  echo "Запустите Р7 Офис Десктоп, не забудьте установить лицензию"
- ;;
+  ;;
 ######### Конец блока кода для Альт
 esac
+set +e
+set +o pipefail +e
